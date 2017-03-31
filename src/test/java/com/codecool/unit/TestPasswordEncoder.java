f94b9f6eb65501d4c9d87f70cb65ab7a2a570bfe
turbek <turbek93@gmail.com>
2017-03-31 13:34:37 +0200

    Refactor and Fix TestPasswordEncoder with mockito

--- src/test/java/com/codecool/unit/TestPasswordEncoder.java
+++ src/test/java/com/codecool/unit/TestPasswordEncoder.java
@@ -1,40 +1,44 @@
 package com.codecool.unit;
 
 
+import com.codecool.service.PasswordEncoderService;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.mockito.InjectMocks;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 
 import static junit.framework.TestCase.assertTrue;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(MockitoJUnitRunner.class)
 public class TestPasswordEncoder {
 
-    @Autowired
+    @InjectMocks
     private PasswordEncoderService service;
 
-    private String validKey;
-    private String invalidKey;
     private String password;
     private String encodedPassword;
 
     @Before
-    public void setup() {
-        validKey = "validKey";
-        invalidKey = "invalidKey";
+    public void setup() throws NoSuchPaddingException, InvalidKeyException, NoSuchAlgorithmException, IllegalBlockSizeException, BadPaddingException, InvalidAlgorithmParameterException {
         password = "password";
-        encodedPassword = service.encode(password);
+        encodedPassword = service.encrypt(password);
     }
 
     @Test
     public void testPasswordGettingEncrypted() {
-        assertTrue(encodedPassword != password);
+        assertTrue(!encodedPassword.equals(password));
     }
 
     @Test
-    public void testPasswordDecoder() {
-        assertTrue(service.decode(encodedPassword) == password);
+    public void testPasswordDecoder() throws IllegalBlockSizeException, BadPaddingException, InvalidAlgorithmParameterException, InvalidKeyException {
+        assertTrue(service.decrypt(encodedPassword).equals(password));
     }
 }
