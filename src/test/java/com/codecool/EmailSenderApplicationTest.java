575f22d323af4e4df41506399669f1bec56a79bf
turbek <turbek93@gmail.com>
2017-04-06 11:54:12 +0200

    Add @ContextConfiguration annotation to classes

--- src/test/java/com/codecool/EmailSenderApplicationTest.java
+++ src/test/java/com/codecool/EmailSenderApplicationTest.java
@@ -5,12 +5,14 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ContextConfiguration
 public class EmailSenderApplicationTest {
 
 	@Autowired
--- src/test/java/com/codecool/unit/TestPasswordEncoder.java
+++ src/test/java/com/codecool/unit/TestPasswordEncoder.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.crypto.BadPaddingException;
@@ -20,6 +21,7 @@ import static junit.framework.TestCase.assertTrue;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
+@ContextConfiguration
 public class TestPasswordEncoder {
 
     @Autowired
