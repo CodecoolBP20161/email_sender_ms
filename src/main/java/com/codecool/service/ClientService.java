56611f6fdefa2e3f20061d72aa1da4a393e24d17
dsztanko <szt.dori@gmail.com>
2017-04-10 11:16:11 +0200

    findByApiKey() method added

--- src/main/java/com/codecool/service/ClientService.java
+++ src/main/java/com/codecool/service/ClientService.java
@@ -1,7 +1,17 @@
 package com.codecool.service;
 
+import com.codecool.model.Client;
+import com.codecool.repository.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ClientService {
+
+    @Autowired
+    ClientRepository clientRepository;
+
+    public Client findByApiKey(String apiKey) {
+        return clientRepository.findByApiKey(apiKey);
+    }
 }
